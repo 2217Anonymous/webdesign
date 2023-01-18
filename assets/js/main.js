7d0d180597f78a797ab9d4941674669ec226973e
Venkatesh <32898157+2217Anonymous@users.noreply.github.com>
2023-01-18 15:29:53 +0530

    fix-dynamic nav-links

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,29 @@ function navbarFixed(){
     })
 }
 
+function setMenuAcive(){
+    const section = document.querySelectorAll("section")
+    const navLinks = document.querySelectorAll(".header--menu__item > a")
+
+    window.addEventListener("scroll",() => {
+        let current =""
+        section.forEach(section => {
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+            // console.log(sectionTop-sectionHeight,sectionTop,sectionHeight,window.pageYOffset);
+            if(pageYOffset >= sectionTop - sectionHeight/3){
+                current = section.getAttribute("id")
+            }
+        })
+
+        navLinks.forEach(li => {
+            li.classList.remove("active")
+            if(current == li.getAttribute("href").split("#")[1]){
+                li.classList.add("active")
+            }
+        })
+    });
+}
+
 navbarFixed()
+setMenuAcive()
